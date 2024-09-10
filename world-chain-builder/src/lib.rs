362ba22bb346969728d8c3108f69cfea1b214b42
Eric Woolsey <ewoolsey@ualberta.ca>
2024-09-10 15:13:21 -0700

    nullifier table

--- world-chain-builder/src/lib.rs
+++ world-chain-builder/src/lib.rs
@@ -3,5 +3,6 @@ pub mod builder;
 pub mod envelope;
 pub mod network;
 pub mod node;
+pub mod nullifier;
 pub mod pool;
 pub mod txpool;
---
+++ world-chain-builder/src/nullifier.rs
@@ -0,0 +1,30 @@
+use reth_db::table::{Compress, Decompress, Table};
+use reth_primitives::B256;
+use serde::{Deserialize, Serialize};
+
+/// Using key value with a prefix would also probablly work here.
+impl Table for NullifierTable {
+    const NAME: &'static str = "Nullifiers";
+
+    type Key = B256;
+
+    type Value = EmptyValue;
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct NullifierTable {}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct EmptyValue;
+
+impl Decompress for EmptyValue {
+    fn decompress<B: AsRef<[u8]>>(_: B) -> Result<Self, reth_db::DatabaseError> {
+        Ok(Self)
+    }
+}
+
+impl Compress for EmptyValue {
+    type Compressed = Vec<u8>;
+
+    fn compress_to_buf<B: reth_primitives::bytes::BufMut + AsMut<[u8]>>(self, _buf: &mut B) {}
+}
