64662429ee0b44d4d1a5cccc9c3d0be269b66c0c
Eric Woolsey <ewoolsey@ualberta.ca>
2024-09-20 11:46:52 -0700

    semaphore proof decode encode tests

--- world-chain-builder/src/pbh/semaphore.rs
+++ world-chain-builder/src/pbh/semaphore.rs
@@ -12,10 +12,11 @@ pub struct Proof(pub semaphore::protocol::Proof);
 impl Decodable for Proof {
     fn decode(buf: &mut &[u8]) -> alloy_rlp::Result<Self> {
         let bytes = ProofBytes::decode(buf)?;
-        if bytes.len() != LEN {
-            return Err(alloy_rlp::Error::UnexpectedLength);
-        }
+        // if bytes.len() != LEN {
+        //     return Err(alloy_rlp::Error::UnexpectedLength);
+        // }
         let fields: [[u8; 32]; 8] = bytemuck::cast(bytes);
+        // panic!("here");
         let a = (fields[0].into(), fields[1].into());
         let b = (
             [fields[2].into(), fields[3].into()],
@@ -45,16 +46,47 @@ impl Encodable for Proof {
     }
 
     fn length(&self) -> usize {
-        LEN
+        LEN + 3
     }
 }
 
 #[derive(Clone, Debug, RlpEncodable, RlpDecodable, PartialEq, Eq)]
 pub struct SemaphoreProof {
-    pub root: Field,
-    pub signal_hash: Field,
-    pub nullifier_hash: Field,
-    pub external_nullifier_hash: Field,
-    pub proof: Proof,
     pub external_nullifier: String,
+    pub external_nullifier_hash: Field,
+    pub nullifier_hash: Field,
+    pub signal_hash: Field,
+    pub root: Field,
+    pub proof: Proof,
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+    use ethers_core::types::U256;
+
+    #[test]
+    fn encode_decode() {
+        let proof = Proof(semaphore::protocol::Proof(
+            (U256::from(1u64), U256::from(2u64)),
+            (
+                [U256::from(3u64), U256::from(4u64)],
+                [U256::from(5u64), U256::from(6u64)],
+            ),
+            (U256::from(7u64), U256::from(8u64)),
+        ));
+        let semaphore_proof = SemaphoreProof {
+            external_nullifier: "0-012025-11".to_string(),
+            external_nullifier_hash: Field::from(9u64),
+            nullifier_hash: Field::from(10u64),
+            signal_hash: Field::from(11u64),
+            root: Field::from(12u64),
+            proof,
+        };
+        let encoded = alloy_rlp::encode(&semaphore_proof);
+        let mut buf = encoded.as_slice();
+        let decoded = SemaphoreProof::decode(&mut buf).unwrap();
+        assert_eq!(semaphore_proof, decoded);
+    }
 }
