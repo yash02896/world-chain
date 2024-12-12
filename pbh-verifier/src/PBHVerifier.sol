010c3cd834a0e75ad8a6229d169a24bd938fd03a
Eric Woolsey <ewoolsey@ualberta.ca>
2024-12-11 19:22:28 -0800

    rename

--- pbh-verifier/src/PBHVerifier.sol
+++ pbh-verifier/src/PBHVerifier.sol
@@ -1,149 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ByteHasher} from "./helpers/ByteHasher.sol";
-import {PBHExternalNullifier} from "./helpers/PBHExternalNullifier.sol";
-import {IWorldIDGroups} from "@world-id-contracts/interfaces/IWorldIDGroups.sol";
-import {WorldIDImpl} from "@world-id-contracts/abstract/WorldIDProxy.sol";
-import "@BokkyPooBahsDateTimeLibrary/BokkyPooBahsDateTimeLibrary.sol";
+import {WorldIDProxy} from "@world-id-contracts/abstract/WorldIDProxy.sol";
 
-/// @title PBH Verifier Implementation Version 1
+/// @title PBH Verifier
 /// @author Worldcoin
-/// @notice An implementation of a batch-based identity manager for the WorldID protocol.
-/// @dev This is the implementation delegated to by a proxy.
-contract PBHVerifierImplV1 is WorldIDImpl {
-    
+/// @notice An implementation of an on chain PBH verifier.
+contract PBHVerifier is WorldIDProxy {
     ///////////////////////////////////////////////////////////////////////////////
-    ///                   A NOTE ON IMPLEMENTATION CONTRACTS                    ///
+    ///                    !!!! DO NOT ADD MEMBERS HERE !!!!                    ///
     ///////////////////////////////////////////////////////////////////////////////
 
-    // This contract is designed explicitly to operate from behind a proxy contract. As a result,
-    // there are a few important implementation considerations:
-    //
-    // - All updates made after deploying a given version of the implementation should inherit from
-    //   the latest version of the implementation. This prevents storage clashes.
-    // - All functions that are less access-restricted than `private` should be marked `virtual` in
-    //   order to enable the fixing of bugs in the existing interface.
-    // - Any function that reads from or modifies state (i.e. is not marked `pure`) must be
-    //   annotated with the `onlyProxy` and `onlyInitialized` modifiers. This ensures that it can
-    //   only be called when it has access to the data in the proxy, otherwise results are likely to
-    //   be nonsensical.
-    // - This contract deals with important data for the PBH system. Ensure that all newly-added
-    //   functionality is carefully access controlled using `onlyOwner`, or a more granular access
-    //   mechanism.
-    // - Do not assign any contract-level variables at the definition site unless they are
-    //   `constant`.
-    //
-    // Additionally, the following notes apply:
-    //
-    // - Initialisation and ownership management are not protected behind `onlyProxy` intentionally.
-    //   This ensures that the contract can safely be disposed of after it is no longer used.
-    // - Carefully consider what data recovery options are presented as new functionality is added.
-    //   Care must be taken to ensure that a migration plan can exist for cases where upgrades
-    //   cannot recover from an issue or vulnerability.
-
     ///////////////////////////////////////////////////////////////////////////////
-    ///                    !!!!! DATA: DO NOT REORDER !!!!!                     ///
+    ///                             CONSTRUCTION                                ///
     ///////////////////////////////////////////////////////////////////////////////
 
-    // To ensure compatibility between upgrades, it is exceedingly important that no reordering of
-    // these variables takes place. If reordering happens, a storage clash will occur (effectively a
-    // memory safety error).
-
-    using ByteHasher for bytes;
-
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  ERRORS                                ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    /// @notice Thrown when attempting to reuse a nullifier
-    error InvalidNullifier();
-
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  Events                                ///
-    //////////////////////////////////////////////////////////////////////////////
-    
-    /// @notice Emitted when a verifier is updated in the lookup table.
+    /// @notice Constructs a new instance of the PBH Verifier.
+    /// @dev This constructor is only called once, and can be called with the encoded call necessary
+    ///      to initialize the logic contract.
     ///
-    /// @param nullifierHash The nullifier hash that was used.
-    event PBH(
-        uint256 indexed nullifierHash
-    );
-
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  Vars                                  ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    /// @dev The World ID group ID (always 1)
-    uint256 internal immutable GROUP_ID = 1;
-
-    /// @dev The World ID instance that will be used for verifying proofs
-    IWorldIDGroups internal immutable worldId;
-
-    /// @dev Make this configurable
-    uint8 internal immutable numPbhPerMonth;
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  Mappings                              ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
-    mapping(uint256 => bool) internal nullifierHashes;
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    ///                                  Functions                             ///
-    //////////////////////////////////////////////////////////////////////////////
-
-    /// @param _worldId The WorldID instance that will verify the proofs
-    constructor(
-        IWorldIDGroups _worldId,
-        uint8 _numPbhPerMonth
-    ) {
-        worldId = _worldId;
-        numPbhPerMonth = _numPbhPerMonth;
-    }
-
-    /// @param root The root of the Merkle tree (returned by the JS widget).
-    /// @param sender The root of the Merkle tree (returned by the JS widget).
-    /// @param nonce The root of the Merkle tree (returned by the JS widget).
-    /// @param callData The root of the Merkle tree (returned by the JS widget).
-    /// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
-    /// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
-    function verifyPbhProof(
-        uint256 root,
-        address sender,
-        uint256 nonce,
-        bytes memory callData,
-        uint256 pbhExternalNullifier,
-        uint256 nullifierHash,
-        uint256[8] memory proof
-    ) external {
-        // First, we make sure this person hasn't done this before
-        if (nullifierHashes[nullifierHash]) revert InvalidNullifier();
-
-        // We now generate the signal hash from the sender, nonce, and calldata
-        uint256 signalHash = abi.encodePacked(
-            sender,
-            nonce,
-            callData
-        ).hashToField();
-
-        // Verify the external nullifier
-        PBHExternalNullifier.verify(pbhExternalNullifier, numPbhPerMonth);
-         
-        // We now verify the provided proof is valid and the user is verified by World ID
-        worldId.verifyProof(
-            root,
-            GROUP_ID,
-            signalHash,
-            nullifierHash,
-            pbhExternalNullifier,
-            proof
-        );
-
-        // We now record the user has done this, so they can't do it again (proof of uniqueness)
-        nullifierHashes[nullifierHash] = true;
-
-        emit PBH(nullifierHash);
+    /// @param _logic The initial implementation (delegate) of the contract that this acts as a proxy
+    ///        for.
+    /// @param _data If this is non-empty, it is used as the data for a `delegatecall` to `_logic`.
+    ///        This is usually an encoded function call, and allows for initialising the storage of
+    ///        the proxy in a way similar to a traditional solidity constructor.
+    constructor(address _logic, bytes memory _data) payable WorldIDProxy(_logic, _data) {
+        // !!!! DO NOT PUT PROGRAM LOGIC HERE !!!!
+        // It should go in the `initialize` function of the delegate instead.
     }
 }
