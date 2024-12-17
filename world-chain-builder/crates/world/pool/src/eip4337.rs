b4ff6dbafb16cabbffca0024eec980854ce0e977
0xOsiris <djosiris@proton.me>
2024-12-17 09:47:11 -0700

    update signal hash computation

--- world-chain-builder/crates/world/pool/src/eip4337.rs
+++ world-chain-builder/crates/world/pool/src/eip4337.rs
@@ -8,12 +8,7 @@ pub fn hash_user_op(user_op: &PackedUserOperation) -> Field {
     let keccak_hash = keccak256(SolValue::abi_encode_packed(&(
         &user_op.sender,
         &user_op.nonce,
-        &user_op.initCode,
         &user_op.callData,
-        &user_op.accountGasLimits,
-        &user_op.preVerificationGas,
-        &user_op.gasFees,
-        &user_op.paymasterAndData,
     )));
 
     hash_to_field(keccak_hash.as_slice())
