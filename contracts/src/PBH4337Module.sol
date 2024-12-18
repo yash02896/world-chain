b079686cf75e295ffde8115c01bbcc4e5c707908
karankurbur <karankurbur@gmail.com>
2024-12-18 13:26:55 -0800

    fmt

--- contracts/src/PBH4337Module.sol
+++ contracts/src/PBH4337Module.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import {Safe4337Module} from "@4337/Safe4337Module.sol";
 
 contract PBHSafe4337Module is Safe4337Module {
-
     address public immutable PBH_SIGNATURE_AGGREGATOR;
 
     constructor(address _safe, address _pbhSignatureAggregator) Safe4337Module(_safe) {
