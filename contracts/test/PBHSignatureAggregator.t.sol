2c2188e620e84d28803e3f464b54f7b12caa23e3
0xOsiris <djosiris@proton.me>
2024-12-19 11:44:43 -0700

    test: fix aggregate signatures test

--- contracts/test/PBHSignatureAggregator.t.sol
+++ contracts/test/PBHSignatureAggregator.t.sol
@@ -33,18 +33,26 @@ contract PBHSignatureAggregatorTest is Setup {
 
     function createUOTestData() public view returns (PackedUserOperation[] memory) {
         PackedUserOperation[] memory uOps = new PackedUserOperation[](1);
-
         bytes memory proofData = abi.encode(proof);
-        uint256 proofLength = proofData.length;
         bytes memory sigBuffer = new bytes(65);
-        bytes memory signature = new bytes(65 + proofLength);
-        console.log("Proof Length: ", proofLength);
-        assembly {
-            mstore(add(signature, 65), sigBuffer)
-            mstore(add(signature, add(65, proofLength)), proofData)
+        bytes memory signature = new bytes(417);
+        assembly ("memory-safe") {
+            mstore(signature, sigBuffer)
+            mstore(add(signature, 32), mload(add(sigBuffer, 32)))
+            mstore(add(signature, 64), mload(add(sigBuffer, 64)))
+            mstore(add(signature, 65), mload(proofData))
+            mstore(add(add(signature, 65), 32), mload(add(proofData, 32)))
+            mstore(add(add(signature, 65), 64), mload(add(proofData, 64)))
+            mstore(add(add(signature, 65), 96), mload(add(proofData, 96)))
+            mstore(add(add(signature, 65), 128), mload(add(proofData, 128)))
+            mstore(add(add(signature, 65), 160), mload(add(proofData, 160)))
+            mstore(add(add(signature, 65), 192), mload(add(proofData, 192)))
+            mstore(add(add(signature, 65), 224), mload(add(proofData, 224)))
+            mstore(add(add(signature, 65), 256), mload(add(proofData, 256)))
+            mstore(add(add(signature, 65), 288), mload(add(proofData, 288)))
+            mstore(add(add(signature, 65), 320), mload(add(proofData, 320)))
         }
 
-        console.logBytes(signature);
         uOps[0] = PackedUserOperation({
             sender: address(safe),
             nonce: 0,
