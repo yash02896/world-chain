db628997194fd7bf341912b068bae41f5fc7764b
0xKitsune <0xkitsune@protonmail.com>
2024-12-31 14:22:53 -0500

    update initialization tests

--- contracts/test/PBHEntryPointImplV1Init.t.sol
+++ contracts/test/PBHEntryPointImplV1Init.t.sol
@@ -1,42 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import {IWorldIDGroups} from "@world-id-contracts/interfaces/IWorldIDGroups.sol";
-import {MockWorldIDGroups} from "./mocks/MockWorldIDGroups.sol";
-import {CheckInitialized} from "@world-id-contracts/utils/CheckInitialized.sol";
-import {WorldIDImpl} from "@world-id-contracts/abstract/WorldIDImpl.sol";
-import {ByteHasher} from "@helpers/ByteHasher.sol";
-import {IPBHEntryPoint} from "../src/interfaces/IPBHEntryPoint.sol";
-import {IWorldID} from "../src/interfaces/IWorldID.sol";
-import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
-import {PBHEntryPointImplV1} from "../src/PBHEntryPointImplV1.sol";
-import {PBHEntryPoint} from "../src/PBHEntryPoint.sol";
+// import {CheckInitialized} from "../../utils/CheckInitialized.sol";
 import {TestSetup} from "./TestSetup.sol";
-import {TestUtils} from "./TestUtils.sol";
-import "@helpers/PBHExternalNullifier.sol";
+import {IWorldIDGroups} from "@world-id-contracts/interfaces/IWorldIDGroups.sol";
 
-/// @title PBHVerifer Verify Tests
-/// @notice Contains tests for the pbhVerifier
+import {CheckInitialized} from "@world-id-contracts/utils/CheckInitialized.sol";
+import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {WorldIDImpl} from "@world-id-contracts/abstract/WorldIDImpl.sol";
+
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {PBHEntryPoint} from "../src/PBHEntryPoint.sol";
+import {PBHEntryPointImplV1} from "../src/PBHEntryPointImplV1.sol";
+import {IPBHEntryPoint} from "../src/interfaces/IPBHEntryPoint.sol";
+import {IMulticall3} from "../src/interfaces/IMulticall3.sol";
+import {IWorldID} from "../src/interfaces/IWorldID.sol";
+
+/// @title PBHEntryPointImplV1InitTest
+/// @notice Contains logic asserting the correct initialization of the PBHEntryPointImplV1 contract
 /// @author Worldcoin
-contract PBHEntryPointImplV1Test is TestSetup {
-    using ByteHasher for bytes;
 
-    event PBH(address indexed sender, IPBHEntryPoint.PBHPayload payload);
-    event NumPbhPerMonthSet(uint8 indexed numPbhPerMonth);
-    event WorldIdSet(address indexed worldId);
+contract PBHEntryPointImplV1InitTest is TestSetup {
+    IPBHEntryPoint uninitializedPBHEntryPoint;
 
-    // TODO: move this to test utils
-    /// @notice Test payload for the PBHVerifier
-    IPBHEntryPoint.PBHPayload public testPayload = IPBHEntryPoint.PBHPayload({
-        root: 1,
-        pbhExternalNullifier: TestUtils.getValidPBHExternalNullifier(0),
-        nullifierHash: 1,
-        proof: [uint256(0), 0, 0, 0, 0, 0, 0, 0]
-    });
-
-    uint256 internal nonce = 1;
-    address internal sender = address(0x123);
-    bytes internal testCallData = hex"deadbeef";
+    function setup() public {
+        pbhEntryPointImpl = address(new PBHEntryPointImplV1());
+        uninitializedPBHEntryPoint = IPBHEntryPoint(address(new PBHEntryPoint(pbhEntryPointImpl, new bytes(0x0))));
+    }
 
     function test_initialize(IWorldID worldId, IEntryPoint entryPoint, uint8 numPbh, address multicall) public {
         vm.assume(address(worldId) != address(0) && address(entryPoint) != address(0) && multicall != address(0));
@@ -109,59 +101,51 @@ contract PBHEntryPointImplV1Test is TestSetup {
         pbhEntryPoint.initialize(worldId, entryPoint, numPbh, multicall);
     }
 
-    // TODO:
-    function test_verifyPbh() public {}
+    function test_verifyPbh_RevertIf_Uninitialized() public {
+        IPBHEntryPoint.PBHPayload memory pbhPayload = IPBHEntryPoint.PBHPayload({
+            root: 1,
+            pbhExternalNullifier: 0,
+            nullifierHash: 1,
+            proof: [uint256(0), 0, 0, 0, 0, 0, 0, 0]
+        });
 
-    // TODO:
-    function test_verifyPbh_RevertIf_InvalidNullifier() public {}
-
-    // TODO: verify proof if worldid addr is set?
-
-    // TODO:
-    function test_handleAggregatedOps() public {}
-
-    // TODO:
-    function test_valdiateSignaturesCallback() public {}
-
-    // TODO:
-    function test_validateSignaturesCallback_RevertIf_IncorrectHashedOps() public {}
-
-    // TODO:
-    function test_pbhMulticall() public {}
-
-    function test_setNumPbhPerMonth(uint8 numPbh) public {
-        vm.assume(numPbh > 0);
-
-        vm.prank(OWNER);
-        vm.expectEmit(true, true, true, true);
-        emit NumPbhPerMonthSet(numPbh);
-        pbhEntryPoint.setNumPbhPerMonth(numPbh);
+        vm.expectRevert(CheckInitialized.ImplementationNotInitialized.selector);
+        uninitializedPBHEntryPoint.verifyPbh(0, pbhPayload);
     }
 
-    function test_setNumPbhPerMonth_RevertIf_NotOwner(uint8 numPbh) public {
-        vm.expectRevert("Ownable: caller is not the owner");
-        pbhEntryPoint.setNumPbhPerMonth(numPbh);
+    function test_handleAggregatedOps_RevertIf_Uninitialized() public {
+        IEntryPoint.UserOpsPerAggregator[] memory opsPerAggregator;
+        address payable beneficiary = payable(address(0));
+
+        vm.expectRevert(CheckInitialized.ImplementationNotInitialized.selector);
+        uninitializedPBHEntryPoint.handleAggregatedOps(opsPerAggregator, beneficiary);
     }
 
-    function test_setNumPbhPerMonth_RevertIf_InvalidNumPbhPerMonth() public {
-        vm.prank(OWNER);
-        vm.expectRevert(PBHEntryPointImplV1.InvalidNumPbhPerMonth.selector);
-        pbhEntryPoint.setNumPbhPerMonth(0);
+    function test_validateSignaturesCallback_RevertIf_Uninitialized() public {
+        vm.expectRevert(CheckInitialized.ImplementationNotInitialized.selector);
+        uninitializedPBHEntryPoint.validateSignaturesCallback(bytes32(0));
     }
 
-    function test_setWorldId(address addr) public {
-        vm.assume(addr != address(0));
+    function test_pbhMulticall_RevertIf_Uninitialized() public {
+        IMulticall3.Call3[] memory calls;
+        IPBHEntryPoint.PBHPayload memory pbhPayload = IPBHEntryPoint.PBHPayload({
+            root: 1,
+            pbhExternalNullifier: 0,
+            nullifierHash: 1,
+            proof: [uint256(0), 0, 0, 0, 0, 0, 0, 0]
+        });
 
-        vm.prank(OWNER);
-        vm.expectEmit(true, false, false, false);
-        emit WorldIdSet(addr);
-        pbhEntryPoint.setWorldId(addr);
+        vm.expectRevert(CheckInitialized.ImplementationNotInitialized.selector);
+        uninitializedPBHEntryPoint.pbhMulticall(calls, pbhPayload);
     }
 
-    function test_setWorldId_RevertIf_NotOwner(address addr) public {
-        vm.expectRevert("Ownable: caller is not the owner");
-        pbhEntryPoint.setWorldId(addr);
+    function test_setNumPbhPerMonth_RevertIf_Uninitialized() public {
+        vm.expectRevert(CheckInitialized.ImplementationNotInitialized.selector);
+        uninitializedPBHEntryPoint.setNumPbhPerMonth(30);
     }
 
-    // TODO: only init and onlyproxy tests?
+    function test_setWorldId_RevertIf_Uninitialized() public {
+        vm.expectRevert(CheckInitialized.ImplementationNotInitialized.selector);
+        uninitializedPBHEntryPoint.setWorldId(address(0));
+    }
 }
